5d086d635cb2917ae1f3a75dfc630f75eb2baa92
Austin Pritchett <austin@pritchett.info>
2020-12-13 16:02:03 -0600

    Update 3318Project.sql

--- 3318Project.sql
+++ 3318Project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 13, 2020 at 07:55 PM
+-- Generation Time: Dec 13, 2020 at 10:58 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -37,6 +37,16 @@ CREATE TABLE `Editors` (
   `zipCode` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `Editors`
+--
+
+INSERT INTO `Editors` (`firstName`, `lastName`, `address`, `ContactNumber`, `city`, `state`, `zipCode`) VALUES
+('Chris', 'Sawyer', '123 Main Street', '1234567890', 'Austin', 'Texas', '90210'),
+('Tracy', 'Goldstein', '266 High Meadow St', '5704959454', 'Pittston', 'Pennsylvania', '18640'),
+('James', 'Bauch', '2650 Freedom Ln', '6192005508', 'Hornitos', 'California', '95325'),
+('George', 'Hotz', '1414 Meyer St', '7134645124', 'Victoria', 'Texas', '77856');
+
 -- --------------------------------------------------------
 
 --
@@ -51,6 +61,16 @@ CREATE TABLE `Locations` (
   `state` longtext DEFAULT NULL,
   `zipCode` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Locations`
+--
+
+INSERT INTO `Locations` (`name`, `address`, `ContactNumber`, `city`, `state`, `zipCode`) VALUES
+('Houston Galleria', '5085 Westheimer Rd', '7136220663', 'Houston', 'Texas', '77056'),
+('Pine Sol', '9410 Fuqua', '7139879479', 'Houston', 'TX', '77075'),
+('Rock Peak', '845 Watters Rd', '8451784343', 'Philadelphia', 'Pennstlvania', '71333'),
+('Green Park', '843 Windy Blvd', '8479128143', 'Pasadena', 'Texas', '77034');
 
 -- --------------------------------------------------------
 
@@ -74,7 +94,10 @@ CREATE TABLE `Models` (
 --
 
 INSERT INTO `Models` (`firstName`, `lastName`, `age`, `address`, `ContactNumber`, `city`, `state`, `zipCode`) VALUES
-('Austin', 'Pritchett', 22, '9538 Misty Vale Ln', '8324093442', 'Houston', 'Texas', '77075');
+('Terry', 'Issacs', 33, '3485 Davisson St', '7657619281', 'Indianapolis', 'Indiana', '46226'),
+('Olivia', 'Maxwell', 20, '4705 Heavner Avenue', '7707760811', 'Norcross', 'Georgia', '30071'),
+('Austin', 'Pritchett', 22, '9538 Misty Vale Ln', '8324093442', 'Houston', 'Texas', '77075'),
+('James', 'Henson', 24, '1380 Lunch Rd', '9209945212', 'Random Lake', 'Wisconsin', '53075');
 
 -- --------------------------------------------------------
 
@@ -91,6 +114,16 @@ CREATE TABLE `Photographers` (
   `state` longtext DEFAULT NULL,
   `zipCode` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Photographers`
+--
+
+INSERT INTO `Photographers` (`firstName`, `lastName`, `address`, `ContactNumber`, `city`, `state`, `zipCode`) VALUES
+('John', 'Moore', '14 Golly Ave', '13e	4132238696', 'Chicago', 'Illinois', '44474'),
+('Univ', 'ofHous', '1 Main St', '7132218000', 'Houston', 'Texas', '77002'),
+('Philip', 'Sherman', '42 Wallaby Way', '8674443919', 'Sydney', 'California', '91434'),
+('Darth', 'Vador', '999 Resistance Way', '9999999999', 'Space City', 'Texas', '11111');
 
 --
 -- Indexes for dumped tables
